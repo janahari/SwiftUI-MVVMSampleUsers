39f42a4c82be82ad813386379339a5a2db278670
janahari <jana.hariprasad525@gmail.com>
2025-11-13 04:24:23 +0530

    Inital commit- Swift UI MVVVM

---
+++ MVVMCoordinator/MVVMCoordinator/MVVMCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMCoordinatorApp.swift
+//  MVVMCoordinator
+//
+//  Created by apple on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

66da8521b4207abc2a3c0eed3d3abaae2b2b3c45
janahari <jana.hariprasad525@gmail.com>
2025-11-13 21:07:41 +0530

    first com

---
+++ SwiftConcurrent/DoTryCatchBootCamp/DoTryCatchBootCamp/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  DoTryCatchBootCamp
+//
+//  Created by apple on 13/11/25.
+//
+
+import SwiftUI
+
+class DoTryCatchViewModel: ObservableObject {
+    @Published var title = "Hello"
+    func fetchTitle() {
+        
+    }
+}
+
+struct ContentView: View {
+    @StateObject private var viewModel: DoTryCatchViewModel = DoTryCatchViewModel()
+    var body: some View {
+        VStack {
+            
+            Text(viewModel.title)
+                .frame(width: 300,height: 300)
+                .background(.blue)
+                .onTapGesture {
+                    viewModel.fetchTitle()
+                }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftConcurrent/DoTryCatchBootCamp/DoTryCatchBootCamp/DoTryCatchBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTryCatchBootCampApp.swift
+//  DoTryCatchBootCamp
+//
+//  Created by apple on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DoTryCatchBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

bf3beb9973404edb7f8f28f553504994f5788285
janahari <jana.hariprasad525@gmail.com>
2025-11-14 09:24:00 +0530

    Added Toucple returning function

--- SwiftConcurrent/DoTryCatchBootCamp/DoTryCatchBootCamp/ContentView.swift
+++ SwiftConcurrent/DoTryCatchBootCamp/DoTryCatchBootCamp/ContentView.swift
@@ -7,11 +7,30 @@
 
 import SwiftUI
 
+class DoTryCatchDataManager {
+    let isActive: Bool = true
+    func getTitle() -> (title: String?, error: Error?) {
+        if false {
+        return ("NEW TEXT", nil)
+        } else {
+            return (nil,URLError(URLError.badURL))
+        }
+       
+    }
+}
 class DoTryCatchViewModel: ObservableObject {
     @Published var title = "Hello"
+    let manager = DoTryCatchDataManager()
     func fetchTitle() {
+        let returnedData = manager.getTitle()
+        if let title = returnedData.title {
+            self.title = title
+        } else if let error = returnedData.error {
+            self.title = error.localizedDescription
+        }
         
     }
+    
 }
 
 struct ContentView: View {

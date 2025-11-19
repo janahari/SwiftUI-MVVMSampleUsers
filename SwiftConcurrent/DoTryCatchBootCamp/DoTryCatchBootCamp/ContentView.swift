74111e9b9f79d9c3d38c404d2e8c2f730c3a50a3
janahari <jana.hariprasad525@gmail.com>
2025-11-19 08:51:20 +0530

    updated try catch

--- SwiftConcurrent/DoTryCatchBootCamp/DoTryCatchBootCamp/ContentView.swift
+++ SwiftConcurrent/DoTryCatchBootCamp/DoTryCatchBootCamp/ContentView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 class DoTryCatchDataManager {
-    let isActive: Bool = true
+    let isActive: Bool = false
+    
     func getTitle() -> (title: String?, error: Error?) {
         if false {
         return ("NEW TEXT", nil)
@@ -24,26 +25,57 @@ class DoTryCatchDataManager {
         }
         
     }
+    
+    func getTitle3() throws -> String {
+        if isActive {
+            return "NEW TEXT"
+        } else {
+            throw URLError(.badServerResponse)
+        }
+    }
+    func getTitle4() throws -> String {
+        if isActive {
+            return "NEW TEXT from Title 4 "
+        } else {
+            throw URLError(.badServerResponse)
+        }
+    }
 }
 
 class DoTryCatchViewModel: ObservableObject {
     @Published var title = "Hello"
     let manager = DoTryCatchDataManager()
     func fetchTitle() {/*
-        let returnedData = manager.getTitle()
-        if let title = returnedData.title {
-            self.title = title
-        } else if let error = returnedData.error {
-            self.title = error.localizedDescription
-        }
+                        let returnedData = manager.getTitle()
+                        if let title = returnedData.title {
+                        self.title = title
+                        } else if let error = returnedData.error {
+                        self.title = error.localizedDescription
+                        }
                         */
-        let result = manager.getTitle2()
+        /*
+         let result = manager.getTitle2()
         switch result {
         case .success(let title):
             self.title = title
+            self.title = title
         case .failure(let error):
             self.title = error.localizedDescription
+         }
+         */
+        do {
+            let newTitle = try manager.getTitle3()
+            self.title = newTitle
+            print("Setting the title 4 ")
+            let finalTitle = try manager.getTitle4()
+            self.title = finalTitle
+/* We have used multiple throw functions if any function is caught exception  then it will return immediately and other functions will not execute */
+
+        } catch let error {
+            self.title = error.localizedDescription
         }
+        
+        
     }
 }
 
